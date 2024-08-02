c7c8aec50dc3a25096ee5832780705d560f30ea4
Bharathi Chinnusamy <bharu@Bharathis-Air.attlocal.net>
2024-08-01 22:55:39 -0400

    added code for creating profile for user

--- app.js
+++ app.js
@@ -7,36 +7,81 @@ app.use(bodyParser.json());
 
 const db = mysql.createConnection({ host: 'localhost', user: 'root', password: 'mylife84', database: 'newdatabase' })
 
-app.post('/create', (req, res) => {
-    // console.log(req.body)
-    const { receiver, giver, amount, gift_year } = req.body
-
-    if (!receiver || !giver || !amount || !gift_year) {
-        return res.status(400).send('reveiver,giver,amount and gift_year are required')
+app.post('/create/profiles', (req, res) => {
+    const { first_Name, last_Name, email } = req.body
+    if (!first_Name || !last_Name || !email) {
+        return res.status(400).send({ error: 'firstname,lastname and email are required' })
     }
-
-    const checkquery = `SELECT * FROM gift_users WHERE giver=?`
-    db.query(checkquery, [giver], (err, result) => {
+    const checkExistedProfile = `SELECT * FROM gift_users_profile WHERE first_Name=? AND last_Name=?`
+    db.query(checkExistedProfile, [first_Name, last_Name], (err, result) => {
         if (err) {
-            res.status(500).send('error checking for existing error')
+            console.log(err)
+            res.status(400).send({ message: 'error checking profile' })
         }
-        if (result.length > 0) {
-            res.status(409).send('The giver is already existed')
+        else if (result.length >0) {
+            res.status(200).send({ message: 'The profile you are trying to create is already existed' })
         }
         else {
-            const insertQuery = `INSERT INTO gift_users (receiver,giver,amount,gift_year) VALUES (?,?,?,?)`;
-            db.query(insertQuery, [receiver, giver, amount, gift_year], (err, result) => {
+            const insertData = `INSERT INTO gift_users_profile(first_Name,last_Name,email) VALUES(?,?,?)`
+            db.query(insertData, [first_Name, last_Name, email], (err, result) => {
                 if (err) {
-                    return res.status(500).send('error inserting data')
+                    console.log(err)
+                    return res.status(500).send({ message: 'error creating profile' })
                 }
-                // console.log(result.insertId)
-                res.status(201).send({ message: 'data created successfully', id: result.insertId })
-            });
+                // console.log(result)
+                res.status(201).send({ message: 'profile created successfully' })
+            })
         }
     })
 })
 
-app.get('/', (req, res) => {
+app.post('/create/data', (req, res) => {
+    const { giver_Id, receiver_Id, amount } = req.body
+
+    if (!giver_Id || !receiver_Id || !amount) {
+        return res.status(400).send({ error: 'giverId,reveiverId and amount are required' })
+    }
+    else {
+        const insertQuery = `INSERT INTO gift_users_data (giver_Id,receiver_Id,amount) VALUES (?,?,?)`;
+        db.query(insertQuery, [giver_Id, receiver_Id, amount], (err, result) => {
+            if (err) {
+                return res.status(500).send({ error: 'Foreign Key Error', message: 'The provided id does not exist in the referenced table. Please ensure to use a valid id' })
+            }
+            res.status(201).send({ message: 'data created successfully' })
+        })
+    }
+})
+
+// app.post('/data', (req, res) => {
+//     // console.log(req.body)
+//     const { receiver, giver, amount, gift_year } = req.body
+
+//     if (!receiver || !giver || !amount || !gift_year) {
+//         return res.status(400).send('reveiver,giver,amount and gift_year are required')
+//     }
+
+//     const checkquery = `SELECT * FROM gift_users WHERE receiver=? AND giver=?`
+//     db.query(checkquery, [receiver, giver], (err, result) => {
+//         if (err) {
+//             res.status(500).send('error checking for existing error')
+//         }
+//         if (result.length > 0) {
+//             res.status(409).send('The giver is already existed')
+//         }
+//         else {
+//             const insertQuery = `INSERT INTO gift_users (receiver,giver,amount,gift_year) VALUES (?,?,?,?)`;
+//             db.query(insertQuery, [receiver, giver, amount, gift_year], (err, result) => {
+//                 if (err) {
+//                     return res.status(500).send('error inserting data')
+//                 }
+//                 // console.log(result.insertId)
+//                 res.status(201).send({ message: 'data created successfully', id: result.insertId })
+//             });
+//         }
+//     })
+// })
+
+app.get('/profiles', (req, res) => {
 
     const readQuery = `SELECT * FROM  gift_users`
     db.query(readQuery, (err, result) => {
@@ -47,7 +92,7 @@ app.get('/', (req, res) => {
     })
 })
 
-app.put('/update/:id', (req, res) => {
+app.put('/profiles/:id', (req, res) => {
     // console.log(req.body)
     const { amount } = req.body
 
@@ -65,7 +110,7 @@ app.put('/update/:id', (req, res) => {
     })
 })
 
-app.delete('/delete/:id', (req, res) => {
+app.delete('/profiles/:id', (req, res) => {
     // console.log(req.params.id)
     const deleteParticularQuery = `DELETE FROM gift_users WHERE id=${req.params.id}`
     db.query(deleteParticularQuery, (err, result) => {
--- mysql.js
+++ mysql.js
@@ -10,70 +10,89 @@ db.connect((err) => {
   console.log('Connected to the MySQL database.');
 
 });
-const createTableQuery = `
-    CREATE TABLE gift_users (
-      id INT AUTO_INCREMENT PRIMARY KEY,
-      giver VARCHAR(255) NOT NULL ,
-      receiver VARCHAR(255) NOT NULL,
-      amount INT NOT NULL,
-      gift_year YEAR NOT NULL
-  )`
-db.query(createTableQuery, (err, results,) => {
-  if (err) {
-    console.log('Error creating table:', err);
-    return;
-  }
-  console.log('Table created successfully:', results);
-});
+const createProfileTableQuery = `CREATE TABLE gift_users_profile(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  First_Name VARCHAR(255) NOT NULL,
+  Last_Name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  created_At TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_At TIMESTAMP DEFAULT CURRENT_TIMESTAMP )`
 
-const insertQuery = `INSERT INTO gift_users (giver,receiver,amount,) VALUES ('Bharathi','Nyra',500)`;
-db.query(insertQuery, (err, results) => {
+db.query(createProfileTableQuery, (err, result) => {
   if (err) {
-    console.log('Error inserting data:', err);
+    console.log("error creating giftUsersProfileTable")
     return;
   }
-  console.log('Data inserted successfully:');
-});
+  else {
+    console.log("Table created")
+  }
 
-const readQuery = `SELECT * FROM  gift_users`
-db.query(readQuery, (err, result) => {
-  if (err) {
-    console.log('error reading data', err)
-    return;
-  }
-  console.log('read the data successfully', result)
 })
 
+// const createTableQuery = `
+//     CREATE TABLE gift_users (
+//       id INT AUTO_INCREMENT PRIMARY KEY,
+//       giver VARCHAR(255) NOT NULL ,
+//       receiver VARCHAR(255) NOT NULL,
+//       amount INT NOT NULL,
+//       gift_year YEAR NOT NULL
+//   )`
+// db.query(createTableQuery, (err, results,) => {
+//   if (err) {
+//     console.log('Error creating table:', err);
+//     return;
+//   }
+//   console.log('Table created successfully:', results);
+// });
 
-const readParticularQuery = ` SELECT* FROM gift_users WHERE id=2`
-db.query(readParticularQuery, (err, result) => {
-  if (err) {
-    console.log('error reading the data', err)
-    return;
-  }
-  console.log('read the data successfully', result)
-})
+// const insertQuery = `INSERT INTO gift_users (giver,receiver,amount,) VALUES ('Bharathi','Nyra',500)`;
+// db.query(insertQuery, (err, results) => {
+//   if (err) {
+//     console.log('Error inserting data:', err);
+//     return;
+//   }
+//   console.log('Data inserted successfully:');
+// });
+
+// const readQuery = `SELECT * FROM  gift_users`
+// db.query(readQuery, (err, result) => {
+//   if (err) {
+//     console.log('error reading data', err)
+//     return;
+//   }
+//   console.log('read the data successfully', result)
+// })
 
 
-const updateParticularQuery = `UPDATE gift_users SET giver='Kabi' WHERE id=2`
-db.query(updateParticularQuery, (err, result) => {
-  if (err) {
-    console.log('error updating data', err)
-    return;
-  }
-  console.log('updated the data successfully', result)
-})
+// const readParticularQuery = ` SELECT* FROM gift_users WHERE id=2`
+// db.query(readParticularQuery, (err, result) => {
+//   if (err) {
+//     console.log('error reading the data', err)
+//     return;
+//   }
+//   console.log('read the data successfully', result)
+// })
 
-const deleteParticularQuery = `DELETE FROM gift_users WHERE id=15`
-db.query(deleteParticularQuery, (err, result) => {
-  if (err) {
-    console.log('error deleting data', err)
-    return;
-  }
-  console.log('deleted the data successfully', result)
-})
-// Close the connection
-connection.end();
 
-;
+// const updateParticularQuery = `UPDATE gift_users SET giver='Kabi' WHERE id=2`
+// db.query(updateParticularQuery, (err, result) => {
+//   if (err) {
+//     console.log('error updating data', err)
+//     return;
+//   }
+//   console.log('updated the data successfully', result)
+// })
+
+// const deleteParticularQuery = `DELETE FROM gift_users WHERE id=15`
+// db.query(deleteParticularQuery, (err, result) => {
+//   if (err) {
+//     console.log('error deleting data', err)
+//     return;
+//   }
+//   console.log('deleted the data successfully', result)
+// })
+// // Close the connection
+// connection.end();
+
+
 
